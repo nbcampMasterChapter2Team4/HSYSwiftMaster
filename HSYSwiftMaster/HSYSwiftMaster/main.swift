ce1498457ba28e0774e5d92c5d4c4140fc2501a7
seoyounghan <happyunbd364@naver.com>
2025-03-19 19:23:59 +0900

    feat: [#3] 필수문제 3

--- HSYSwiftMaster/HSYSwiftMaster/main.swift
+++ HSYSwiftMaster/HSYSwiftMaster/main.swift
@@ -52,3 +52,25 @@ let myMapResult = myMap([1, 2, 3, 4, 5]) {
 }
 
 print(result)
+
+// MARK: - 필수 문제 3
+
+func a(_ aArray: [Int]) -> [Int] {
+    return aArray.enumerated().filter { $0.offset % 2 == 0 }.map { $0.element }
+}
+
+print(a([1, 2, 3, 4, 5]))
+
+func b(_ bArray: [String]) -> [String] {
+    return bArray.enumerated().filter { $0.offset % 2 == 0 }.map { $0.element }
+}
+
+print(b(["가", "나", "다", "라", "마"]))
+
+func c<T>(_ cArray: [T]) -> [T] {
+    return cArray.enumerated().filter { $0.offset % 2 == 0 }.map { $0.element }
+}
+
+print(c([1, 2, 3, 4, 5]))
+print(c(["가", "나", "다", "라", "마"]))
+
